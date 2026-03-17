e5153472389491d9cebb72c77bef339d2d7e573b
Human-Gechi <kimberlydaniel363@gmail.com>
2026-03-17 08:28:15 +0100

    infra

---
+++ infrastructure/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+    description = "Region for Infra"
+    type = string
+    default = "value"
+
+}
+
+variable "profile" {
+  description = "Profile name"
+  type = string
+  default = "Supply-Chain"
+}
+
+variable "project_name" {
+    description = "Project name"
+    type = string
+    default = "SupplyChain360"
+
+}
