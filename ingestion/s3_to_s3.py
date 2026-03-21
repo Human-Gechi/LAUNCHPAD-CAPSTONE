b91b43ca6c6c31999503f63df2ad6b784f0c5016
Human-Gechi <kimberlydaniel363@gmail.com>
2026-03-21 01:30:22 +0100

    ingestion

--- ingestion/s3_to_s3.py
+++ ingestion/s3_to_s3.py
@@ -1,5 +1,6 @@
 import boto3
 import sys
+import os
 
 from dotenv import load_dotenv
 
@@ -8,8 +9,20 @@ sys.path
 
 load_dotenv()
 
+import boto3
 
-s3 = boto3.resource("s3")
-for bucket in s3.buckets.all():
-    print(bucket.name)
-    ingest_logger.info("Buckets list")
+# Source bucket credentials
+SRC_ACCESS_KEY = os.getenv("SOURCE_ACCESS_KEY")
+SRC_SECRET_KEY = os.getenv("SOURCE_SECRET_KEY")
+SRC_BUCKET = os.getenv("source-bucket-name")
+
+# Destination bucket credentials
+DST_ACCESS_KEY = os.getenv("DESTINATION_ACCESS_KEY")
+DST_SECRET_KEY = os.getenv("DESTINATION_SECRET_KEY")
+DST_BUCKET = os.getenv("destination-bucket-name")
+OBJECT_KEY = os.getenv("your-object-key")
+
+class MoveData():
+    def __init__(self,source, destination):
+        self.source = source
+        self.destination = destination
