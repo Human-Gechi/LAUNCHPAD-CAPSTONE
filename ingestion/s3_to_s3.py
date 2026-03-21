1ea48f54c33f09235701f58a5ba84c0140804fb7
Human-Gechi <kimberlydaniel363@gmail.com>
2026-03-21 02:03:15 +0100

    ingestion

--- ingestion/s3_to_s3.py
+++ ingestion/s3_to_s3.py
@@ -1,28 +1,48 @@
 import boto3
-import sys
 import os
 
 from dotenv import load_dotenv
 
 from log import ingest_logger
-sys.path
 
+# Load environment variables
 load_dotenv()
 
-import boto3
+# S3 client
+class S3ClientFactory:
+    @staticmethod
+    def create_client(access_key, secret_key, region):
+        return boto3.client(
+            "s3",
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            region_name=region
+        )
 
-# Source bucket credentials
+# Get credentials from env. file
 SRC_ACCESS_KEY = os.getenv("SOURCE_ACCESS_KEY")
 SRC_SECRET_KEY = os.getenv("SOURCE_SECRET_KEY")
-SRC_BUCKET = os.getenv("source-bucket-name")
+SRC_BUCKET = os.getenv("SOURCE_BUCKET")
+SRC_REGION = os.getenv("SOURCE_REGION")
 
-# Destination bucket credentials
 DST_ACCESS_KEY = os.getenv("DESTINATION_ACCESS_KEY")
 DST_SECRET_KEY = os.getenv("DESTINATION_SECRET_KEY")
-DST_BUCKET = os.getenv("destination-bucket-name")
-OBJECT_KEY = os.getenv("your-object-key")
+DST_BUCKET = os.getenv("DESTINATIN_BUCKET")
+DST_REGION = os.getenv("DESTINATION_REGION")
+OBJECT_KEY = os.getenv("OBJECT_KEY")
 
-class MoveData():
-    def __init__(self,source, destination):
-        self.source = source
-        self.destination = destination
+
+src_client = S3ClientFactory.create_client(SRC_ACCESS_KEY, SRC_SECRET_KEY, SRC_REGION)
+dst_client = S3ClientFactory.create_client(DST_ACCESS_KEY, DST_SECRET_KEY, DST_REGION)
+
+class MoveData:
+    def __init__(self, src_client, dst_client, src_bucket, dst_bucket):
+        self.src_client = src_client
+        self.dst_client = dst_client
+        self.src_bucket = src_bucket
+        self.dst_bucket = dst_bucket
+        ingest_logger.info("✅ SRC & DST Buckets initialization")
+
+    def copy_object(self, object_key):
+        obj = self.src_client.get_object(Bucket=self.src_bucket, Key=object_key)
+        ingest_logger.info("✅ SRC Bucket initialization")
