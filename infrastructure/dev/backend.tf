dd379ad790ad3f9a58cabb22c9faacc980e95fa5
Human-Gechi <kimberlydaniel363@gmail.com>
2026-03-21 02:35:05 +0100

    removing infra

--- infrastructure/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "SupplyChain360"
-    key = "env/key/terraform.tfstate"
-    region = "eu-north-1"
-    use_lockfile = true
-    profile = "dev-Supply-Chain"
-  }
-}
