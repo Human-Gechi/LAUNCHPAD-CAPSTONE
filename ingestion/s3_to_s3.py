d0ff120c40c983e82407d6e29c42d9d3aa010c87
Human-Gechi <kimberlydaniel363@gmail.com>
2026-03-21 03:09:09 +0100

    ingestion

--- ingestion/s3_to_s3.py
+++ ingestion/s3_to_s3.py
@@ -8,16 +8,13 @@ from log import ingest_logger
 # Load environment variables
 load_dotenv()
 
+
 # S3 client
 class S3ClientFactory:
     @staticmethod
     def create_client(access_key, secret_key, region):
-        return boto3.client(
-            "s3",
-            aws_access_key_id=access_key,
-            aws_secret_access_key=secret_key,
-            region_name=region
-        )
+        return boto3.client("s3", aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name=region)
+
 
 # Get credentials from env. file
 SRC_ACCESS_KEY = os.getenv("SOURCE_ACCESS_KEY")
@@ -35,6 +32,7 @@ OBJECT_KEY = os.getenv("OBJECT_KEY")
 src_client = S3ClientFactory.create_client(SRC_ACCESS_KEY, SRC_SECRET_KEY, SRC_REGION)
 dst_client = S3ClientFactory.create_client(DST_ACCESS_KEY, DST_SECRET_KEY, DST_REGION)
 
+
 class MoveData:
     def __init__(self, src_client, dst_client, src_bucket, dst_bucket):
         self.src_client = src_client
@@ -43,6 +41,9 @@ class MoveData:
         self.dst_bucket = dst_bucket
         ingest_logger.info("✅ SRC & DST Buckets initialization")
 
-    def copy_object(self, object_key):
-        obj = self.src_client.get_object(Bucket=self.src_bucket, Key=object_key)
-        ingest_logger.info("✅ SRC Bucket initialization")
+    def copy_object(self):
+        pass
+
+
+if __name__ == "__main__":
+    mover = MoveData(src_client, dst_client, SRC_BUCKET, DST_BUCKET)
---
+++ ingestion/utils.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from pathlib import Path
+from log import ingest_logger
+
+# Get Timestamp
+def _time_stamp():
+    """Timestamp Function"""
+    return datetime.now().strftime("%Y-%m-%d")
+
+#File path for files
+def file_path(path: str, extension: str):
+    """ "Function to convert files to parquet"""
+    time = _time_stamp()
+    filename = f"{path}_{time}.{extension}"
+    ingest_logger.info(f"✅ Parquet file {filename} created")
+    return Path(filename)
+ingestion/utils.py
+ingestion\write.py
