b46075734c7ecb70d38eaafe46051ab1de931d1d
Ogechukwu Okoli <kimberlydaniel363@gmail.com>
2026-04-07 15:10:49 +0100

    Update clean_decimal.sql

--- dbt_SupplyChain360/macros/clean_decimal.sql
+++ dbt_SupplyChain360/macros/clean_decimal.sql
@@ -1,3 +1,4 @@
+# Macro for formatting decimals in my models 
 {% macro clean_decimal(column_name, default_val=0.0) %}
     coalesce(abs(cast({{ column_name }} as decimal(10,2))), {{ default_val }})
 {% endmacro %}
